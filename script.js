5bbf79136ad19af3da3dd120d138da19832381b8
Daniela <daniela.fedyakin@gmail.com>
2020-08-25 23:27:35 +0200

    add favicon and meta property tags

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const getSeoData = () => {
+    const pageTitle = document.title
+    const pageUrl = document.URL
+    const pageDescription = document.querySelector('meta[name="description"]').content
+    const pageImg = document.querySelectorAll('img')[0].src
+    
+    setOgSeoData({ pageTitle, pageUrl, pageDescription, pageImg })
+}
+
+const setOgSeoData = ({ pageTitle, pageUrl, pageDescription, pageImg }) => {
+    document.querySelector('meta[property="og:url"]').content = pageUrl
+    document.querySelector('meta[property="og:title"]').content = pageTitle
+    document.querySelector('meta[property="og:site_name"]').content = pageTitle
+    document.querySelector('meta[property="og:description"]').content = pageDescription
+    document.querySelector('meta[property="og:image"]').content = pageImg
+}
+
+getSeoData()
 /*
  * Add here any JavaScript you want to use
  */
